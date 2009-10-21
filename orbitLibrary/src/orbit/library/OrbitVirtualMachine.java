454ece02c4dd3e6dfcc99e8688dc970599f9cf33
jorge.schrauwen <jorge.schrauwen@e0c530b8-b125-11de-af8f-1f60b1e8fe01>
2009-10-21 11:14:04 +0000

    added power functions to orbitvm

--- orbitLibrary/src/orbit/library/OrbitVirtualMachine.java
+++ orbitLibrary/src/orbit/library/OrbitVirtualMachine.java
@@ -19,134 +19,221 @@ public class OrbitVirtualMachine {
     VirtualMachine vm;
 
     public OrbitVirtualMachine(VirtualMachine vm) {
-	this.vm = vm;
+        this.vm = vm;
     }
 
     public VirtualMachine getVirtualMachine() {
-	return vm;
+        return vm;
     }
 
     public String getName() {
-	return vm.getName();
+        return vm.getName();
     }
 
     public VirtualMachinePowerState getPowerState() {
-	return vm.getRuntime().getPowerState();
+        return vm.getRuntime().getPowerState();
     }
 
     public String getGuestState() {
-	// local
-	GuestInfo gi = this.getGuestInfo();
+        // local
+        GuestInfo gi = this.getGuestInfo();
 
-	return gi.getGuestState();
+        return gi.getGuestState();
     }
 
     public GuestInfo getGuestInfo() {
-	return vm.getGuest();
+        return vm.getGuest();
     }
 
     public boolean getToolsInstalled() {
-	// local
-	GuestInfo gi = this.getGuestInfo();
+        // local
+        GuestInfo gi = this.getGuestInfo();
 
-	return !(gi.getToolsStatus() == VirtualMachineToolsStatus.toolsNotInstalled);
+        return !(gi.getToolsStatus() == VirtualMachineToolsStatus.toolsNotInstalled);
     }
 
     public boolean getToolsRunning() {
-	// local
-	GuestInfo gi = this.getGuestInfo();
+        // local
+        GuestInfo gi = this.getGuestInfo();
 
-	return !(gi.getToolsStatus() == VirtualMachineToolsStatus.toolsOk);
+        return !(gi.getToolsStatus() == VirtualMachineToolsStatus.toolsOk);
     }
 
     public boolean getToolsUpgradable() {
-	// local
-	GuestInfo gi = this.getGuestInfo();
+        // local
+        GuestInfo gi = this.getGuestInfo();
 
-	return (gi.getToolsStatus() == VirtualMachineToolsStatus.toolsOld);
+        return (gi.getToolsStatus() == VirtualMachineToolsStatus.toolsOld);
     }
 
     public boolean getToolsUnmanaged() {
-	// local
-	GuestInfo gi = this.getGuestInfo();
+        // local
+        GuestInfo gi = this.getGuestInfo();
 
-	return gi.getToolsVersionStatus().equals("guestToolsUnmanaged");
+        return gi.getToolsVersionStatus().equals("guestToolsUnmanaged");
     }
 
     public String getGuestOSName() {
-	// locals
-	String os = "unknown";
-	GuestInfo gi = this.getGuestInfo();
+        // locals
+        String os = "unknown";
+        GuestInfo gi = this.getGuestInfo();
 
-	os = gi.getGuestFullName();
-	if (os == null || os.isEmpty()) {
-	    os = gi.getGuestFamily();
-	}
-	if (os == null | os.isEmpty()) {
-	    os = gi.getGuestId();
-	}
+        os = gi.getGuestFullName();
+        if (os == null || os.isEmpty()) {
+            os = gi.getGuestFamily();
+        }
+        if (os == null | os.isEmpty()) {
+            os = gi.getGuestId();
+        }
 
-	return os;
+        return os;
     }
 
     public String getGuestHostName() {
-	// local
-	GuestInfo gi = this.getGuestInfo();
+        // local
+        GuestInfo gi = this.getGuestInfo();
 
-	return gi.getHostName();
+        return gi.getHostName();
     }
 
     public String getGuestPrimaryIP() {
-	// local
-	GuestInfo gi = this.getGuestInfo();
+        // local
+        GuestInfo gi = this.getGuestInfo();
 
-	return gi.getIpAddress();
+        return gi.getIpAddress();
     }
 
     public String[] getGuestIPs() {
-	// local
-	ArrayList result = new ArrayList();
-	GuestInfo gi = this.getGuestInfo();
-	GuestNicInfo gni[] = gi.getNet();
+        // local
+        ArrayList result = new ArrayList();
+        GuestInfo gi = this.getGuestInfo();
+        GuestNicInfo gni[] = gi.getNet();
 
-	// loops ip's
-	for (GuestNicInfo nic : gni) {
-	    for (String ip : nic.getIpAddress()) {
-		result.add(ip);
-	    }
-	}
+        // loops ip's
+        for (GuestNicInfo nic : gni) {
+            for (String ip : nic.getIpAddress()) {
+                result.add(ip);
+            }
+        }
 
-	return (String[]) result.toArray();
+        return (String[]) result.toArray();
     }
 
     public HostSystem getHost() {
-	// locals
-	HostSystem host;
-	Folder rootFolder;
+        // locals
+        HostSystem host;
+        Folder rootFolder;
 
-	try {
-	    // locals
-	    ManagedObjectReference hostMOR;
+        try {
+            // locals
+            ManagedObjectReference hostMOR;
 
-	    rootFolder = vm.getServerConnection().getServiceInstance().getRootFolder();
-	    ManagedEntity[] mes = new InventoryNavigator(rootFolder).searchManagedEntities("HostSystem");
-	    if (mes == null || mes.length == 0) {
-		throw new NullPointerException();
-	    }
+            rootFolder = vm.getServerConnection().getServiceInstance().getRootFolder();
+            ManagedEntity[] mes = new InventoryNavigator(rootFolder).searchManagedEntities("HostSystem");
+            if (mes == null || mes.length == 0) {
+                throw new NullPointerException();
+            }
 
-	    host = null;
-	    hostMOR = vm.getRuntime().getHost();
-	    for (int i = 0; i < mes.length; i++) {
-		if (mes[i].getMOR().equals(hostMOR)) {
-		    host = (HostSystem) mes[i];
-		}
-	    }
-	} catch (Exception e) {
-	    return null;
-	}
+            host = null;
+            hostMOR = vm.getRuntime().getHost();
+            for (int i = 0; i < mes.length; i++) {
+                if (mes[i].getMOR().equals(hostMOR)) {
+                    host = (HostSystem) mes[i];
+                }
+            }
+        } catch (Exception e) {
+            return null;
+        }
 
-	return host;
+        return host;
     }
 
-    //TODO: smart powerOn/powerOff/suspend/reset
+    public boolean powerOn() {
+        // locals
+        com.vmware.vim25.mo.Task t;
+
+        // start vm
+        try {
+
+            if (this.getPowerState() == VirtualMachinePowerState.poweredOn) {
+                return true;
+            } else {
+                t = this.getVirtualMachine().powerOnVM_Task(null);
+                if (t.waitForMe().equalsIgnoreCase(Task.SUCCESS)) {
+                    return true;
+                }
+            }
+        } catch (Exception ex) {
+            return false;
+        }
+        return false;
+    }
+
+    public boolean powerOff() {
+        // locals
+        com.vmware.vim25.mo.Task t;
+
+        // stop vm
+        try {
+            if (this.getPowerState() == VirtualMachinePowerState.poweredOff) {
+                return true;
+            } else if (this.getToolsRunning()) {
+                this.getVirtualMachine().shutdownGuest();
+                return true;
+            } else {
+                t = this.getVirtualMachine().powerOffVM_Task();
+                if (t.waitForMe().equalsIgnoreCase(Task.SUCCESS)) {
+                    return true;
+                }
+
+            }
+        } catch (Exception ex) {
+            return false;
+        }
+        return false;
+    }
+
+    public boolean reset() {
+        // locals
+        com.vmware.vim25.mo.Task t;
+
+        // stop vm
+        try {
+            if (this.getToolsRunning()) {
+                this.getVirtualMachine().rebootGuest();
+                return true;
+            } else {
+                t = this.getVirtualMachine().resetVM_Task();
+                if (t.waitForMe().equalsIgnoreCase(Task.SUCCESS)) {
+                    return true;
+                }
+
+            }
+        } catch (Exception ex) {
+            return false;
+        }
+        return false;
+    }
+
+    public boolean suspend() {
+        // locals
+        com.vmware.vim25.mo.Task t;
+
+        // start vm
+        try {
+            if (this.getPowerState() == VirtualMachinePowerState.suspended) {
+                return true;
+            } else if (this.getPowerState() == VirtualMachinePowerState.poweredOn) {
+                t = this.getVirtualMachine().suspendVM_Task();
+                if (t.waitForMe().equalsIgnoreCase(Task.SUCCESS)) {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        } catch (Exception ex) {
+            return false;
+        }
+        return false;
+    }
 }
